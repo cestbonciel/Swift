14941ba99e2b17d0c301a57cf0162e30424c8153
Nat <cestbonciel88@knou.ac.kr>
2024-01-25 22:17:42 +0900

    chore: modified value

--- Example_TypeMethod/Example_TypeMethod/main.swift
+++ Example_TypeMethod/Example_TypeMethod/main.swift
@@ -7,5 +7,43 @@
 
 import Foundation
 
-print("Hello, World!")
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+    
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
 
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Natalie Kim")
+player.complete(level: 3)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel).")
